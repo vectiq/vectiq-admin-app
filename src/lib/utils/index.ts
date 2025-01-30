0e783b9e54a1faf631baeec86b93fe8cf4fe3f02
Dan <1719975+dbnardi@users.noreply.github.com>
2025-01-30 17:04:16 +1100

    lib deelte

--- src/lib/utils/index.ts
+++
@@ -1,8 +0,0 @@
-// Export utility functions from a single entry point
-export { formatCurrency } from './currency';
-export { formatDate, getCurrentWeekDates } from './date';
-export { cn } from './styles';
-
-export const capitaliseFirstChar = (val: string):string =>{
-    return val.charAt(0).toUpperCase() + val.slice(1);
-}
